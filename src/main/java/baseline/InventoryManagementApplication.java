8b42dd07fa9eaef89694e3bb43cd91400993c18a
Eric Heston <eric.heston98@gmail.com>
2021-11-18 02:35:45 -0500

    first commit

---
+++ src/main/java/baseline/InventoryManagementApplication.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 2 Solution
+ *  Copyright 2021 Eric Heston
+ */
+
+package baseline;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class InventoryManagementApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(InventoryManagementApplication.class.getResource("InventoryManagementApplication.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 603, 624);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/main/java/baseline/InventoryManagementApplicationController.java
@@ -0,0 +1,14 @@
+package baseline;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class InventoryManagementApplicationController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO document why this method is empty
+    }
+}
